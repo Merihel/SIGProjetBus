98bf193b47baf51e04473760eb933f276878a73a
Morgane <morgane0798@hotmail.fr>
2018-12-12 21:30:36 +0100

    [ADD] Export fichier KML

--- index.php
+++ index.php
@@ -24,10 +24,10 @@ function runTest() {
 	$g->addEdge("d", "h", 183);
 	$g->addEdge("h", "j", 167);
 	$g->addEdge("j", "h", 167);
-	
+
 	$g->addEdge("e", "j", 502);
 	$g->addEdge("j", "e", 502);
-	
+
 	$g->addEdge("f", "i", 250);
 	$g->addEdge("i", "f", 250);
 
@@ -35,11 +35,11 @@ function runTest() {
 	$g->addEdge("j", "i", 84);
 
 	list($distances, $prev) = $g->paths_from("a");
-	
+
 	$path = $g->paths_to($prev, "j");
-	
+
 	print_r($path);
-	
+
 }
 //runTest();
 
@@ -53,5 +53,53 @@ class MyDB extends SQLite3
 
 $db = new MyDB();
 
-$result = $db->query('SELECT * FROM GEO_POINT WHERE GEO_POI_ID=1');
-var_dump($result->fetchArray());
+
+
+// CREATION DU FICHIER KML (une pop up de téléchargement s'ouvre au lancement de la page) //
+$result = $db->query('SELECT * FROM GEO_POINT');
+
+$dom = new DOMDocument('1.0', 'UTF-8');
+$node = $dom->createElementNS('http://www.opengis.net/kml/2.2', 'kml'); // ou : $node = $dom->createElementNS('http://earth.google.com/kml/2.1', 'kml');
+$parNode = $dom->appendChild($node);
+$dnode = $dom->createElement('Document');
+$docNode = $parNode->appendChild($dnode);
+
+// OPTION : POUR AJOUTER UN STYLE
+// $restStyleNode = $dom->createElement('Style');
+// $restStyleNode->setAttribute('id', 'restaurantStyle');
+// $restIconstyleNode = $dom->createElement('IconStyle');
+// $restIconstyleNode->setAttribute('id', 'restaurantIcon');
+// $restIconNode = $dom->createElement('Icon');
+// $restHref = $dom->createElement('href', 'http://maps.google.com/mapfiles/kml/pal2/icon63.png');
+// $restIconNode->appendChild($restHref);
+// $restIconstyleNode->appendChild($restIconNode);
+// $restStyleNode->appendChild($restIconstyleNode);
+// $docNode->appendChild($restStyleNode);
+
+
+
+while ($row = $result->fetchArray()) {
+	$node = $dom->createElement('Placemark');
+  $placeNode = $docNode->appendChild($node);
+	$placeNode->setAttribute('id', 'placemark' . $row['GEO_POI_ID']);
+
+	 $nameNode = $dom->createElement('name',htmlentities($row['GEO_POI_NOM']));
+	 $placeNode->appendChild($nameNode);
+	 // optionnel :
+	 // $descNode = $dom->createElement('description', $row['GEO_POI_NOM']);
+ 	 // $placeNode->appendChild($descNode);
+	 // 	 $styleUrl = $dom->createElement('styleUrl', '#restaurantStyle');
+	 // $placeNode->appendChild($styleUrl);
+
+	  $pointNode = $dom->createElement('Point');
+	  $placeNode->appendChild($pointNode);
+
+	  // On crée un élément coordinates et on lui donne la valeur de la longitude et latitude
+	  $coorStr = $row['GEO_POI_LONGITUDE'] . ','  . $row['GEO_POI_LATITUDE'];
+	  $coorNode = $dom->createElement('coordinates', $coorStr);
+	  $pointNode->appendChild($coorNode);
+}
+
+	$kmlOutput = $dom->saveXML();
+	header('Content-type: application/vnd.google-earth.kml+xml');
+	echo $kmlOutput;
